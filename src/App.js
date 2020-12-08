85340fedc69651d0a55e5091b14685cb671f4efc
Tom <ravi.tom@tavant.com>
2020-12-08 07:10:55 +0530

    added the shop item page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
 import { Route, Switch, Link } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = (props) => (
-  <div>
-    <Link to="/">Hats Page {`${props.match.params.productId}`}</Link>
-    <h1>HatsPage</h1>
-    <button onClick={() => props.history.push("/")}>Home Page</button>
-  </div>
-);
+import ShopPage from "./pages/shoppage/shoppage.component";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/hats" component={HatsPage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
   );
